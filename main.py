621e4bd83a9d35c778e281824eabac6fd9ef9e60
XDyma <denisukd751@gmail.com>
2025-11-08 17:27:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from pygame import *
+# інші імпорти
+
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+
+
+WHITE = (255, 255, 255)
+
+
+init()
+screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display.set_caption("НАЗВА ГРИ")
+
+
+running = True
+while running:
+   screen.fill(WHITE)
+
+
+   for e in event.get():
+       if e.type == QUIT:
+           running = False
+
+
+
+
+   display.flip()
+
+
